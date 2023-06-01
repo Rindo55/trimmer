e957b83463a627b5701c2139ddccfc6056fc053e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-06-01 19:04:32 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,20 +4,21 @@ import os
 class Config:
 
     API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+    API_HASH = "96b46175824223a33737657ab943fd6a"
+   
+    BOT_TOKEN = "5410320498:AAGAPfA_Wnl4ZQRpCIWhxQR008O97_aIf7I"
+    SESSION_NAME =  ":memory:"
+    LOG_CHANNEL = -1001557165553
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
-    MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
-    MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
-    TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
-    SLOW_SPEED_DELAY = int(os.environ.get("SLOW_SPEED_DELAY", 5))
-    HOST = os.environ.get("HOST", "")
-    TIMEOUT = int(os.environ.get("TIMEOUT", 60 * 30))
+    AUTH_USERS = 1425489930
+    MAX_PROCESSES_PER_USER = 2
+    MAX_TRIM_DURATION = 600
+    TRACK_CHANNEL = False
+    SLOW_SPEED_DELAY = 5
+    HOST = ""
+    TIMEOUT = 60 * 30
     DEBUG = bool(os.environ.get("DEBUG"))
-    WORKER_COUNT = int(os.environ.get("WORKER_COUNT", 20))
+    WORKER_COUNT = 20
     IAM_HEADER = os.environ.get("IAM_HEADER", "")
 
     COLORS = [
